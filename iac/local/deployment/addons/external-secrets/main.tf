10dd5d30e9f38fca0efcf2801861eeba05f78fab
greyhats13 <imam.arief.rhmn@gmail.com>
2024-12-24 18:14:11 +0700

    remove external-secret

--- iac/local/deployment/addons/external-secrets/main.tf
+++ iac/local/deployment/addons/external-secrets/main.tf
@@ -32,46 +32,46 @@ module "iam_assumable_role_inline_policy" {
   ]
 }
 
-module "argocd_app" {
-  source        = "../../../modules/helm"
-  region        = var.region
-  standard      = local.addon_standard
-  override_name = local.addon_standard.Feature
-  repository    = "https://argoproj.github.io/argo-helm"
-  chart         = "argocd-apps"
-  values        = ["${file("manifest/${local.addon_standard.Feature}.yaml")}"]
-  namespace     = "argocd"
-  dns_name      = "${local.addon_standard.Feature}.${var.unit}.blast.co.id"
-  extra_vars = {
-    argocd_namespace                       = "argocd"
-    source_origin_repoURL                  = "https://charts.external-secrets.io"
-    source_origin_chart                    = local.addon_standard.Feature
-    source_origin_targetRevision           = "0.11.0"
-    source_override_repoURL                = "git@github.com:${var.github_owner}/${var.github_repo}.git"
-    source_override_targetRevision         = "local"
-    source_override_path                   = "charts/local/addons/${local.addon_standard.Feature}/values.yaml"
-    project                                = "default"
-    destination_server                     = "https://kubernetes.default.svc"
-    destination_namespace                  = local.addon_standard.Feature
-    syncPolicy_automated_prune             = true
-    syncPolicy_automated_selfHeal          = true
-    syncPolicy_syncOptions_CreateNamespace = true
-    cluster_secret_store_path              = "charts/local/addons/${local.addon_standard.Feature}/manifest/cluster-secret-store"
-  }
-}
+# module "argocd_app" {
+#   source        = "../../../modules/helm"
+#   region        = var.region
+#   standard      = local.addon_standard
+#   override_name = local.addon_standard.Feature
+#   repository    = "https://argoproj.github.io/argo-helm"
+#   chart         = "argocd-apps"
+#   values        = ["${file("manifest/${local.addon_standard.Feature}.yaml")}"]
+#   namespace     = "argocd"
+#   dns_name      = "${local.addon_standard.Feature}.${var.unit}.blast.co.id"
+#   extra_vars = {
+#     argocd_namespace                       = "argocd"
+#     source_origin_repoURL                  = "https://charts.external-secrets.io"
+#     source_origin_chart                    = local.addon_standard.Feature
+#     source_origin_targetRevision           = "0.11.0"
+#     source_override_repoURL                = "git@github.com:${var.github_owner}/${var.github_repo}.git"
+#     source_override_targetRevision         = "local"
+#     source_override_path                   = "charts/local/addons/${local.addon_standard.Feature}/values.yaml"
+#     project                                = "default"
+#     destination_server                     = "https://kubernetes.default.svc"
+#     destination_namespace                  = local.addon_standard.Feature
+#     syncPolicy_automated_prune             = true
+#     syncPolicy_automated_selfHeal          = true
+#     syncPolicy_syncOptions_CreateNamespace = true
+#     cluster_secret_store_path              = "charts/local/addons/${local.addon_standard.Feature}/manifest/cluster-secret-store"
+#   }
+# }
 
-resource "kubernetes_secret_v1" "secrets" {
-  metadata {
-    name      = "awssm-secret"
-    namespace = local.addon_standard.Feature
-  }
+# resource "kubernetes_secret_v1" "secrets" {
+#   metadata {
+#     name      = "awssm-secret"
+#     namespace = local.addon_standard.Feature
+#   }
 
-  data = {
-    access-key                  = "test"
-    secret-access-key           = "test"
-    AWS_SECRETSMANAGER_ENDPOINT = "http://localstack.localstack.svc.cluster.local:4566"
-    AWS_SSM_ENDPOINT            = "http://localstack.localstack.svc.cluster.local:4566"
-    AWS_STS_ENDPOINT            = "http://localstack.localstack.svc.cluster.local:4566"
-  }
-  depends_on = [module.argocd_app]
-}
+#   data = {
+#     access-key                  = "test"
+#     secret-access-key           = "test"
+#     AWS_SECRETSMANAGER_ENDPOINT = "http://localstack.localstack.svc.cluster.local:4566"
+#     AWS_SSM_ENDPOINT            = "http://localstack.localstack.svc.cluster.local:4566"
+#     AWS_STS_ENDPOINT            = "http://localstack.localstack.svc.cluster.local:4566"
+#   }
+#   depends_on = [module.argocd_app]
+# }
