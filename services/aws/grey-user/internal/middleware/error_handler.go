51292f705e4b94b5807b777f82e9328c939c378d
greyhats13 <imam.arief.rhmn@gmail.com>
2025-01-02 00:33:21 +0700

    update fiber error handler

--- services/aws/grey-user/internal/middleware/error_handler.go
+++ services/aws/grey-user/internal/middleware/error_handler.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"runtime/debug"
 
-	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -21,15 +20,6 @@ func CustomErrorHandler(c *fiber.Ctx, err error) error {
 	var e *fiber.Error
 	if errors.As(err, &e) {
 		code = e.Code
-		var re *awshttp.ResponseError
-		if errors.As(err, &re) {
-			// Return a JSON response consistent for all errors
-			debug.PrintStack()
-			return c.Status(re.HTTPStatusCode()).JSON(fiber.Map{
-				"success": false,
-				"message": err.Error(),
-			})
-		}
 	}
 
 	// Example: Log stacktrace if it's a 5xx error
